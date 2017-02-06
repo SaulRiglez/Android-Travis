5dc7cf3ff9da5c7b3ec5b32f521ad7978db6f3e1
SaulRiglez <saul.riglez@gmail.com>
2017-02-06 16:44:18 -0500

    added helo hedgehoge

--- app/src/main/java/com/example/travissimple/MainActivity.java
+++ app/src/main/java/com/example/travissimple/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: ");
+        Log.d(TAG, "onCreate: Hello hanfry hedged hooe ");
         //Change test
         //Commit test
     }
